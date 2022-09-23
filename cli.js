1cae2de79477b212325a1fd9fef5352f4e585c12
root <root@bdhack-pc.localdomain>
2022-09-23 18:33:29 -0400

    Added more to cli.js.

---
+++ cli.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+//help stuff
+
+
+//require moment-timezone
+const moment-timezone = require('moment-timezone');
+
+//import moment from the package
+const timezone = moment.tz.guest();
+
+//URL builder
+
+//fetch API call
+
+//response text
+const days = args.d
+
+if (days == 0) {
+	console/log("today.")
+} else if (days > 1) {
+	console.log("in " + days + " days.")
+} else {
+	console.log("tomorrow.")
+}
